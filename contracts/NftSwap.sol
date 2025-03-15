7449e33ecdc79d856c11579078a74fca750ef020
Jeramai <j.faber@provrex.nl>
2025-03-15 11:14:55 +0700

    Added mock NFTs to trade and the agree function

--- contracts/NftSwap.sol
+++ contracts/NftSwap.sol
@@ -1,40 +1,102 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 contract NftSwap {
     enum TradeStatus {
         Proposed,
         Agreed,
         Confirmed,
-        Swapped,
+        Completed,
         Cancelled
     }
 
     struct Trade {
-        address proposer;
-        address offeredNftContract;
-        uint256 offeredNftId;
-        address requestedNftContract;
-        uint256 requestedNftId;
+        address fromAddress;
+        address fromNftContract;
+        uint256 fromNftId;
+        bool fromHasAgreed;
+        bool fromHasConfirmed;
+        
+        address toAddress;
+        address toNftContract;
+        uint256 toNftId;
+        bool toHasAgreed;
+        bool toHasConfirmed;
+
         TradeStatus status;
     }
 
     Trade[] public trades;
 
+    event TradeProposed(uint256 tradeId);
+    event TradeAgreed(uint256 tradeId, address user);
+    event TradeConfirmed(uint256 tradeId, address user);
+    event TradeCompleted(uint256 tradeId);
+    event TradeCancelled(uint256 tradeId);
+    
     function proposeTrade(
-        address _offeredNftContract,
-        uint256 _offeredNftId,
-        address _requestedNftContract,
-        uint256 _requestedNftId
-    ) public {
+        address _fromNftContract,
+        uint256 _fromNftId,
+        address _toNftContract,
+        uint256 _toNftId,
+        address _toAddress
+    ) external returns (uint256) {
+        require(_toAddress != msg.sender, "Cannot trade with yourself");
+        
         Trade memory newTrade = Trade({
-            proposer: msg.sender,
-            offeredNftContract: _offeredNftContract,
-            offeredNftId: _offeredNftId,
-            requestedNftContract: _requestedNftContract,
-            requestedNftId: _requestedNftId,
+            fromAddress: msg.sender,
+            fromNftContract: _fromNftContract,
+            fromNftId: _fromNftId,
+            fromHasAgreed: false,
+            fromHasConfirmed: false,
+            
+            toAddress: _toAddress,
+            toNftContract: _toNftContract,
+            toNftId: _toNftId,
+            toHasAgreed: false,
+            toHasConfirmed: false,
+            
             status: TradeStatus.Proposed
         });
+
         trades.push(newTrade);
+        
+        uint256 tradeId = trades.length - 1;
+        emit TradeProposed(tradeId);
+
+        return tradeId;
+    }   
+
+    function agreeTrade(uint256 _tradeId) external {
+        require(_tradeId < trades.length, "Trade does not exist");
+        Trade storage trade = trades[_tradeId];
+        
+        require(trade.status == TradeStatus.Proposed, "Trade is not in proposed state");
+        require(
+            msg.sender == trade.fromAddress || msg.sender == trade.toAddress,
+            "Not authorized to agree to this trade"
+        );
+
+        if (msg.sender == trade.fromAddress) {
+            require(!trade.fromHasAgreed, "Already agreed to this trade");
+            trade.fromHasAgreed = true;
+        } else {
+            require(!trade.toHasAgreed, "Already agreed to this trade");
+            trade.toHasAgreed = true;
+        }
+
+        // If both parties have agreed, update status
+        if (trade.fromHasAgreed && trade.toHasAgreed) {
+            trade.status = TradeStatus.Agreed;
+        }
+
+        emit TradeAgreed(_tradeId, msg.sender);
     }
-}
+
+    
+    function getTrade(uint256 _tradeId) public view returns (Trade memory) {
+        return trades[_tradeId];
+    }
+} 
--- test/NftSwap.test.ts
+++ test/NftSwap.test.ts
@@ -1,47 +1,91 @@
 import { expect } from 'chai';
 import hre from 'hardhat';
-import { Address, createPublicClient, createWalletClient, getAddress, getContract, http } from 'viem';
-import { hardhat } from 'viem/chains'; // Use hardhat chain
+import { Address, createPublicClient, createWalletClient, getAddress, getContract, http, PublicClient, WalletClient } from 'viem';
+import { hardhat } from 'viem/chains';
+import MockNft from '../artifacts/contracts/MockNft.sol/MockNft.json';
 import NftSwap from '../artifacts/contracts/NftSwap.sol/NftSwap.json';
 
+enum TradeStatus {
+  Proposed,
+  Agreed,
+  Confirmed,
+  Completed,
+  Cancelled
+}
 type Trade = {
-  proposer: Address;
-  offeredNftContract: Address;
-  offeredNftId: bigint;
-  requestedNftContract: Address;
-  requestedNftId: bigint;
-  status: bigint;
+  fromAddress: Address;
+  fromNftContract: Address;
+  fromNftId: bigint;
+  fromHasAgreed: boolean;
+  fromHasConfirmed: boolean;
+
+  toAddress: Address;
+  toNftContract: Address;
+  toNftId: bigint;
+  toHasAgreed: boolean;
+  toHasConfirmed: boolean;
+
+  status: TradeStatus;
 };
 
 describe('NftSwap', function () {
-  it('Should allow a user to propose a trade', async function () {
+  let publicClient: PublicClient;
+  let walletClientOwner: WalletClient;
+  let from: any;
+  let to: any;
+  let nftContract: any;
+  let nftSwap: any;
+
+  const offeredNftId: number = 1,
+    requestedNftId: number = 2;
+
+  beforeEach(async function () {
     // Create Viem clients
-    const publicClient = createPublicClient({
+    publicClient = createPublicClient({
       chain: hardhat, // Use hardhat chain here
       transport: http() // Use the Hardhat Network RPC endpoint
     });
 
     // Get accounts from Hardhat
-    const [owner, addr1, addr2] = await hre.viem.getWalletClients();
+    [from, to] = await hre.viem.getWalletClients();
 
     // Create wallet clients
-    const walletClientOwner = createWalletClient({
+    walletClientOwner = createWalletClient({
       chain: hardhat,
       transport: http(),
-      account: owner.account
+      account: from.account
     });
+
+    // Deploy the NFT contract first
+    const nftHash = await walletClientOwner.deployContract({
+      abi: MockNft.abi,
+      bytecode: MockNft.bytecode as Address,
+      account: from.account,
+      chain: hardhat
+    });
+    const nftReceipt = await publicClient.waitForTransactionReceipt({ hash: nftHash });
+    const nftAddress = nftReceipt.contractAddress!;
 
     // Deploy the contract with Viem
-    const hash = await walletClientOwner.deployContract({
+    const swapHash = await walletClientOwner.deployContract({
       abi: NftSwap.abi,
-      bytecode: NftSwap.bytecode as Address
+      bytecode: NftSwap.bytecode as Address,
+      account: from.account,
+      chain: hardhat
     });
-
-    const receipt = await publicClient.waitForTransactionReceipt({ hash });
+    const receipt = await publicClient.waitForTransactionReceipt({ hash: swapHash });
     const nftSwapAddress = receipt.contractAddress!;
 
     // Get a contract instance with Viem
-    const nftSwap = getContract({
+    nftContract = getContract({
+      address: nftAddress,
+      abi: MockNft.abi,
+      client: {
+        public: publicClient,
+        wallet: walletClientOwner
+      }
+    });
+    nftSwap = getContract({
       address: nftSwapAddress,
       abi: NftSwap.abi,
       client: {
@@ -50,25 +94,71 @@ describe('NftSwap', function () {
       }
     });
 
-    // Arrange
-    const proposer = addr1;
-    const offeredNftContract = getAddress(addr2.account.address); // Use a valid address
-    const offeredNftId = 1;
-    const requestedNftContract = getAddress(owner.account.address); // Use a valid address
-    const requestedNftId = 2;
+    // Mint NFTs to from and to
+    await nftContract.write.mint([from.account.address, offeredNftId]);
+    await nftContract.write.mint([to.account.address, requestedNftId]);
 
+    // Check if the wallet has the NFT
+    const ownerOfferedNft = await nftContract.read.ownerOf([offeredNftId]);
+    const ownerRequestedNft = await nftContract.read.ownerOf([requestedNftId]);
+
+    expect(ownerOfferedNft).to.equal(getAddress(from.account.address));
+    expect(ownerRequestedNft).to.equal(getAddress(to.account.address));
+  });
+
+  it('Should allow a user to propose a trade', async function () {
     // Act
-    const hash2 = await nftSwap.write.proposeTrade([offeredNftContract, offeredNftId, requestedNftContract, requestedNftId], {
-      account: addr1.account
-    });
+    const ownerRequestedNft = await nftContract.read.ownerOf([requestedNftId]);
+    const hash2 = await nftSwap.write.proposeTrade(
+      [getAddress(nftContract.address), offeredNftId, getAddress(nftContract.address), requestedNftId, ownerRequestedNft],
+      { account: from.account }
+    );
 
-    await publicClient.waitForTransactionReceipt({ hash: hash2 }); // Assert
+    // Assert
+    await publicClient.waitForTransactionReceipt({ hash: hash2 });
+
+    // Test
     const trade = (await nftSwap.read.getTrade([0])) as Trade;
-    expect(trade.proposer).to.equal(getAddress(proposer.account.address));
-    expect(trade.offeredNftContract).to.equal(getAddress(offeredNftContract));
-    expect(trade.offeredNftId).to.equal(BigInt(offeredNftId));
-    expect(trade.requestedNftContract).to.equal(getAddress(requestedNftContract));
-    expect(trade.requestedNftId).to.equal(BigInt(requestedNftId));
-    expect(trade.status).to.equal(0); // 0 represents "Proposed"
+    expect(trade.fromAddress).to.equal(getAddress(from.account.address));
+    expect(trade.fromNftContract).to.equal(getAddress(nftContract.address));
+    expect(trade.fromNftId).to.equal(BigInt(offeredNftId));
+    expect(trade.fromHasAgreed).to.equal(false);
+    expect(trade.fromHasConfirmed).to.equal(false);
+    expect(trade.toAddress).to.equal(getAddress(to.account.address));
+    expect(trade.toNftContract).to.equal(getAddress(nftContract.address));
+    expect(trade.toNftId).to.equal(BigInt(requestedNftId));
+    expect(trade.toHasAgreed).to.equal(false);
+    expect(trade.toHasConfirmed).to.equal(false);
+    expect(trade.status).to.equal(TradeStatus.Proposed);
+  });
+  //
+
+  // Get accounts from Hardhat
+  it('Should allow the from of the requested NFT to agree to a trade', async function () {
+    const ownerRequestedNft = await nftContract.read.ownerOf([requestedNftId]);
+    const hash2 = await nftSwap.write.proposeTrade(
+      [getAddress(nftContract.address), offeredNftId, getAddress(nftContract.address), requestedNftId, ownerRequestedNft],
+      { account: from.account }
+    );
+
+    await publicClient.waitForTransactionReceipt({ hash: hash2 });
+
+    // Have FROM side agree
+    const agreeHash = await nftSwap.write.agreeTrade([0], { account: from.account });
+    await publicClient.waitForTransactionReceipt({ hash: agreeHash });
+
+    const tradeFrom = (await nftSwap.read.getTrade([0])) as Trade;
+    expect(tradeFrom.status).to.equal(TradeStatus.Proposed);
+    expect(tradeFrom.fromHasAgreed).to.equal(true);
+    expect(tradeFrom.toHasAgreed).to.equal(false);
+
+    // Have TO side agree
+    const agreeHash2 = await nftSwap.write.agreeTrade([0], { account: to.account });
+    await publicClient.waitForTransactionReceipt({ hash: agreeHash2 });
+
+    const tradeTo = (await nftSwap.read.getTrade([0])) as Trade;
+    expect(tradeTo.status).to.equal(TradeStatus.Agreed);
+    expect(tradeTo.fromHasAgreed).to.equal(true);
+    expect(tradeTo.toHasAgreed).to.equal(true);
   });
 });
